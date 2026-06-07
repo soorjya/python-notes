9f35e194a42076c210a40f60fb67da68fc7ca59d
Soorjyakanta Sethy <99492724+soorjya@users.noreply.github.com>
2026-06-07 12:22:26 +0530

    Create variables_ex.py

---
+++ basics/variables_ex.py
@@ -0,0 +1,11 @@
+# 1. Create Variables
+name = "Soorjya"
+age  = 22
+salary = 26399
+is_emlpoyed = True
+
+# 2. Type check
+print(type(name))     #<class 'str'>
+print(type(age))      #<class 'int'>
+
+# 3. Basic maths
